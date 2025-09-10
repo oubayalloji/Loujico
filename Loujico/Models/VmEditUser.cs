83743b71ae9ea3a955f63c3db39187ae9d27b02d
Fares-Fms <fares.sab2002@gmail.com>
2025-09-10 16:13:55 +0300

    Account

---
+++ Loujico/Models/VmEditUser.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Loujico.Models
+{
+    public class VmEditUser
+    {
+        public string userid { get; set; }
+        [Required]
+        public string username { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string selectedRole { get; set; }
+        [PasswordPropertyText]
+        public string password { get; set; }
+        public List<SelectListItem>? roles { get; set; }
+    }
+}
