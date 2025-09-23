a03daddd30778b036e616daca22f935ad6648899
Oubayalloji <oubayallo@gmail.com>
2025-09-23 11:29:53 +0300

    apply

--- Loujico/Models/CompanySystemContext.cs
+++ Loujico/Models/CompanySystemContext.cs
@@ -24,6 +24,7 @@ public partial class CompanySystemContext : IdentityDbContext<ApplicationUser>
     public virtual DbSet<Co_Industry> Co_Industries { get; set; }
     public  virtual DbSet<Co_Activity> Co_Activities { get; set; }
     public virtual DbSet<Co_Company_Name> Co_Companies { get; set; }
+    public virtual DbSet<Co_CompanyEmployee> Co_CompanyEmployees { get; set; }
     public virtual DbSet<Co_Contact> Co_Contacts { get; set; }
     public virtual DbSet<Co_Legal> Co_Legals { get; set; }
     public virtual DbSet<TbCustomer> TbCustomers { get; set; }
