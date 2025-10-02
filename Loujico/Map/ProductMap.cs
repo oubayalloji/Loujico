8c674bef4408e146c2e484dc48c143f1f73b1b70
Oubayalloji <oubayallo@gmail.com>
2025-10-02 17:07:50 +0300

    file

---
+++ Loujico/Map/ProductMap.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Loujico.Models;
+
+namespace Loujico.Map
+{
+    public class ProductMap:Profile
+    {
+        public ProductMap() 
+        {
+            CreateMap<AddProductModel, TbProduct>()
+            .ForMember(dest => dest.CreatedAt, opt => opt.MapFrom(src => DateTime.UtcNow))
+            .ForMember(dest => dest.IsDeleted, opt => opt.MapFrom(src => false))
+            .ForMember(dest => dest.CreatedBy, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedAt, opt => opt.Ignore())
+            .ForMember(dest => dest.UpdatedBy, opt => opt.Ignore())
+            .ReverseMap();
+        }
+    }
+}
