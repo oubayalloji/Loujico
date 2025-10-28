f088283948b2f64aebce144663fc09ea86eb1c7b
Fares-Fms <fares.sab2002@gmail.com>
2025-10-28 18:19:00 +0300

    First Edit

--- Loujico/BL/ClsSettings.cs
+++ Loujico/BL/ClsSettings.cs
@@ -25,7 +25,9 @@ namespace Loujico.BL
         public Task<bool> AddCity(TbCity City);
         public Task<bool> DeleteCity(int Id);
         public Task<List<object>> GetAllCityType();
+
         public Task<List<object>> GetCityByState(int Id);
+
         public Task<bool> AddCountry(TbCountry Country);
         public Task<bool> DeleteCountry(int Id);
         public Task<List<object>> GetAllCountryType();
